1f0dca4298664d077990ebaa15f775d3f356daf3
Yury Korolev <yury.korolev@gmail.com>
2022-05-13 11:14:03 +0300

    File acessors

--- ios_system.m
+++ ios_system.m
@@ -51,6 +51,35 @@ __thread FILE* thread_stdout;
 __thread FILE* thread_stderr;
 __thread void* thread_context;
 
+FILE* ios_stdin(void) {
+    return thread_stdin;
+}
+
+FILE* ios_stdout(void) {
+    return thread_stdout;
+}
+
+FILE* ios_stderr(void) {
+    return thread_stderr;
+}
+
+int ios_stdinFd(void) {
+    return thread_stdin ? fileno(thread_stdin) : 0;
+}
+
+int ios_stdoutFd(void) {
+    return thread_stdout ? fileno(thread_stdout) : 0;
+}
+
+int ios_stderrFd(void) {
+    return thread_stderr ? fileno(thread_stderr) : 0;
+}
+
+void* ios_context(void) {
+    return thread_context;
+}
+
+
 // Parameters for each session. We can have multiple sessions running in parallel.
 typedef struct _sessionParameters {
     bool isMainThread;   // are we on the first command?
